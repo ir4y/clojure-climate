2f6d0bb7c31f787577e075d45c0e68eab8b12bdf
Ilya Beda <ir4y.ix@gmail.com>
2013-04-18 22:38:36 +0800

    Fix dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [me.raynes/laser "1.1.1"]]
+                 [me.raynes/laser "1.1.1"]
+                 [com.taoensso/carmine "1.6.0"]
+                 [jonase/kibit "0.0.8"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler clojure-climate.handler/app}
   :profiles
   {:dev 
    {:dependencies [
                    [ring-mock "0.1.3"]
-                   [lein-kibit "0.0.8"]
-                   [com.taoensso/carmine "1.6.0"]
                    ]}})
